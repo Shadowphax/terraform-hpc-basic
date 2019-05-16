456adacbd8aa6c9a76fc46e647db66bda9c84c39
Shadowphax <timothy.carr@uct.ac.za>
2019-05-16 23:08:53 +0200

    Added back the security groups. Always seems to give an error about not being able to associate with a subnet during OS instance creation

--- main.tf
+++ main.tf
@@ -3,6 +3,44 @@ resource "openstack_compute_keypair_v2" "authkeys" {
   public_key = "${file("${var.ssh_key_file}.pub")}"
 }
 
+# Security Groups 
+
+resource "openstack_networking_secgroup_v2" "infra_sec_group" {
+  name        = "infra_sec_group"
+  description = "Security group for instances"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "ssh_22" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.infra_sec_group.id}"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "www_80" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 80
+  port_range_max    = 80
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.infra_sec_group.id}"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "icmp" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "icmp"
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.infra_sec_group.id}"
+}
+
+
+## Networking Section ##
+
 resource "openstack_networking_network_v2" "private_net" {
   name           = "private_net"
   admin_state_up = "true"
@@ -27,17 +65,27 @@ resource "openstack_networking_router_interface_v2" "private_router_port" {
   subnet_id = "${openstack_networking_subnet_v2.private_subnet.id}"
 }
 
+
+## Define Floating IP Pool 
 resource "openstack_networking_floatingip_v2" "floating_ip" {
   pool = "${var.pool}"
 }
 
-# Slurm Headnode 
+# Compute Instance - Slurm Headnode 
 resource "openstack_compute_instance_v2" "headnode" {
   name            = "Slurm-Headnode"
   image_name      = "${var.image}"
   flavor_name     = "${var.flavor}"
   key_pair        = "${openstack_compute_keypair_v2.authkeys.name}"
   security_groups = ["${openstack_networking_secgroup_v2.infra_sec_group.name}"]
+ 
+  block_device {
+    uuid                  = "${openstack_blockstorage_volume_v3.slurm_boot.id}"
+    source_type           = "volume"
+    boot_index            = 0
+    destination_type      = "volume"
+    delete_on_termination = true
+  }
 
   network {
     uuid = "${openstack_networking_network_v2.private_net.id}"
@@ -64,6 +112,14 @@ resource "openstack_compute_instance_v2" "slurm_controller" {
   flavor_name     = "${var.flavor}"
   key_pair        = "${openstack_compute_keypair_v2.authkeys.name}"
   security_groups = ["${openstack_networking_secgroup_v2.infra_sec_group.name}"]
+
+  block_device {
+    uuid                  = "${openstack_blockstorage_volume_v3.slurm_ctl_boot.id}"
+    source_type           = "volume"
+    boot_index            = 0
+    destination_type      = "volume"
+    delete_on_termination = false
+  }
 
   network {
     uuid = "${openstack_networking_network_v2.private_net.id}"
