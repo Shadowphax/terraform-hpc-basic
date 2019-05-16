0bf4f4c4e492dc855838faf9864c1b5b8abbe7b0
Shadowphax <timothy.carr@uct.ac.za>
2019-05-16 23:34:30 +0200

    Added image_id so that volumes created with block storage resource provider can set the volume to boot

--- volumes.tf
+++ volumes.tf
@@ -4,7 +4,7 @@ resource "openstack_blockstorage_volume_v3" "slurm_boot" {
   name        = "Slurm_boot"
   description = "Slurm Headnode Boot Volume"
   size        = 5
-  image_id    = "${var.image.id}"
+  image_id    = "${var.image}"
 }
 
 
@@ -14,6 +14,6 @@ resource "openstack_blockstorage_volume_v3" "slurm_ctl_boot" {
   name        = "Slurm_ctl_boot"
   description = "Slurm Controller Boot Volume"
   size        = 5
-  image_id    = "${var.image.id}"
+  image_id    = "${var.image}"
 }
 
