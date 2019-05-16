5d73397adcd92e6fced575cd85826d8a6de08d73
Shadowphax <timothy.carr@uct.ac.za>
2019-05-16 23:20:54 +0200

    Added image_id

--- volumes.tf
+++ volumes.tf
@@ -4,6 +4,7 @@ resource "openstack_blockstorage_volume_v3" "slurm_boot" {
   name        = "Slurm_boot"
   description = "Slurm Headnode Boot Volume"
   size        = 5
+  image_id    = "${var.image.id}"
 }
 
 
@@ -13,5 +14,6 @@ resource "openstack_blockstorage_volume_v3" "slurm_ctl_boot" {
   name        = "Slurm_ctl_boot"
   description = "Slurm Controller Boot Volume"
   size        = 5
+  image_id    = "${var.image.id}"
 }
 
