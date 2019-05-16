8e60d4f5bd623652fe7d9a7e961ae576b2326f99
Shadowphax <timothy.carr@uct.ac.za>
2019-05-16 22:08:16 +0200

    Removed security groups from main.tf and added to security.tf

--- main.tf
+++ main.tf
@@ -27,39 +27,6 @@ resource "openstack_networking_router_interface_v2" "private_router_port" {
   subnet_id = "${openstack_networking_subnet_v2.private_subnet.id}"
 }
 
-resource "openstack_networking_secgroup_v2" "infra_sec_group" {
-  name        = "infra_sec_group"
-  description = "Security group for instances"
-}
-
-resource "openstack_networking_secgroup_rule_v2" "ssh_22" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 22
-  port_range_max    = 22
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = "${openstack_networking_secgroup_v2.infra_sec_group.id}"
-}
-
-resource "openstack_networking_secgroup_rule_v2" "www_80" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 80
-  port_range_max    = 80
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = "${openstack_networking_secgroup_v2.infra_sec_group.id}"
-}
-
-resource "openstack_networking_secgroup_rule_v2" "icmp" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "icmp"
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = "${openstack_networking_secgroup_v2.infra_sec_group.id}"
-}
-
 resource "openstack_networking_floatingip_v2" "floating_ip" {
   pool = "${var.pool}"
 }
