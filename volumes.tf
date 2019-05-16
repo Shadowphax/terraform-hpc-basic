e689741efd4392687d9b06f73daf7861484f3eee
Shadowphax <timothy.carr@uct.ac.za>
2019-05-16 22:17:45 +0200

    volumes.tf

---
+++ volumes.tf
@@ -0,0 +1,17 @@
+# Creation of volumes for Slurm Headnode
+
+resource "openstack_blockstorage_volume_v3" "slurm_boot" {
+  name        = "Slurm_boot"
+  description = "Slurm Headnode Boot Volume"
+  size        = 5
+}
+
+
+# Creation of volumes for Slurm Controller
+
+resource "openstack_blockstorage_volume_v3" "slurm_ctl_boot" {
+  name        = "Slurm_ctl_boot"
+  description = "Slurm Controller Boot Volume"
+  size        = 5
+}
+
