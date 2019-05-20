0790a169c15a82097adf3abc1475ba8cca1b9d37
Shadowphax <timothy.carr@uct.ac.za>
2019-05-20 13:20:49 +0200

    Added additional output field for private address to run playbook on.

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "Slurm Headnode Floating IP Address" {
   value = "${openstack_networking_floatingip_v2.floating_ip.address}"
 }
+
+output " Private IP Address " {
+  value = "${openstack_compute_instance_v2.headnode.network.fixed_ip_v4}"
+}
