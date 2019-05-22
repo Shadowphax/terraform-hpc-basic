6b7e691b4ebb9c6bbcaf5303d3f8e89bc832000c
Shadowphax <timothy.carr@uct.ac.za>
2019-05-23 00:56:08 +0200

    Update name of ssh jumphost

--- inventory.tf
+++ inventory.tf
@@ -6,20 +6,23 @@ data  "template_file" "slurm" {
     vars {
         headnodename = "${openstack_compute_instance_v2.slurm_headnode.name}"
         headnodeip = "${openstack_compute_instance_v2.slurm_headnode.access_ip_v4}"
+        controllername = "${openstack_compute_instance_v2.slurm_controller.name}"
+        controllerip = "${openstack_compute_instance_v2.slurm_controller.access_ip_v4}"
+        username = "${var.ssh_user_name}"
     }
 }
-data  "template_file" "gatewayd" {
-    template = "${file("./templates/gatewayd.tpl")}"
+data  "template_file" "jumpbox" {
+    template = "${file("./templates/jumpbox.tpl")}"
     vars {
         username = "${var.ssh_user_name}"
-        gatewayd_floatingIP = "${openstack_networking_floatingip_v2.floating_ip.address}"
+        jumpbox_floatingIP = "${openstack_networking_floatingip_v2.floating_ip.address}"
     }
 }
 resource "local_file" "slurm_file" {
   content  = "${data.template_file.slurm.rendered}"
   filename = "./inventory/slurm-inventory"
 }
-resource "local_file" "gatewayd_file" {
-  content  = "${data.template_file.gatewayd.rendered}"
-  filename = "./ansible/group_vars/gatewayd.yml"
+resource "local_file" "jumpbox_file" {
+  content  = "${data.template_file.jumpbox.rendered}"
+  filename = "./ansible/group_vars/jumpbox.yml"
 }
