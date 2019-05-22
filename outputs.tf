aafac721dc28fe7374cab69a3f1e02f97be33f9c
Shadowphax <timothy.carr@uct.ac.za>
2019-05-22 18:47:46 +0200

    Removing output variable

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
 output "Slurm Headnode Floating IP Address" {
   value = "${openstack_networking_floatingip_v2.floating_ip.address}"
 }
-
-output "Private IP Address " {
-  value = "${openstack_compute_instance_v2.headnode.access_ip_v4}"
-}
