79433856f4b61b0a0cba994bf70c7b9fc17bdd2a
shadowphax <timothy.carr@uct.ac.za>
2019-05-28 01:23:30 +0200

    Added DNS

--- datasources.tf
+++ datasources.tf
@@ -13,6 +13,7 @@ data  "template_file" "slurm" {
         controllername = "${openstack_compute_instance_v2.slurm_controller.name}"
         controllerip = "${openstack_compute_instance_v2.slurm_controller.access_ip_v4}"
         workernodes = "${join("\n",openstack_compute_instance_v2.slurm_workers.*.name)}"
+        workernodesip = "${join("\n",openstack_compute_instance_v2.slurm_workers.*.access_ip_v4)}"
         jumpbox_floatingIP = "${openstack_networking_floatingip_v2.floating_ip.address}"
         username = "${var.ssh_user_name}"
     }
@@ -20,4 +21,22 @@ data  "template_file" "slurm" {
 resource "local_file" "slurm_file" {
   content  = "${data.template_file.slurm.rendered}"
   filename = "./inventory/slurm-inventory"
+}
+
+data  "template_file" "dns" {
+    template = "${file("./templates/dnsmasq_hosts.tpl")}"
+    vars {
+        headnodename = "${openstack_compute_instance_v2.slurm_headnode.name}"
+        headnodeip = "${openstack_compute_instance_v2.slurm_headnode.access_ip_v4}"
+        controllername = "${openstack_compute_instance_v2.slurm_controller.name}"
+        controllerip = "${openstack_compute_instance_v2.slurm_controller.access_ip_v4}"
+        workernodes = "${join("\n",openstack_compute_instance_v2.slurm_workers.*.name)}"
+        workernodesip = "${join("\n",openstack_compute_instance_v2.slurm_workers.*.access_ip_v4)}"
+        jumpbox_floatingIP = "${openstack_networking_floatingip_v2.floating_ip.address}"
+        username = "${var.ssh_user_name}"
+    }
+}
+resource "local_file" "dnsmasq_hosts" {
+  content = "${data.template_file.dns.rendered}"
+  filename = "./inventory/hosts"
 }
