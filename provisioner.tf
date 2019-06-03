1eeb977650cb9330046bb3a56773b5ce8f521893
shadowphax <timothy.carr@uct.ac.za>
2019-06-03 15:20:56 +0200

    Changed the name of the null_resources

--- provisioner.tf
+++ provisioner.tf
@@ -1,19 +1,19 @@
 // Local Provisioners
-resource "null_resource" "controller-ansible-deploy" {
+resource "null_resource" "controller-deploy" {
   provisioner "local-exec" {
     command = "ansible-playbook -i inventory/slurm-inventory ansible/controller.yml --become"
   }
   depends_on = [openstack_compute_floatingip_associate_v2.headnode_floating_ip]
 }
 
-resource "null_resource" "headnode-ansible-deploy" {
+resource "null_resource" "headnode-deploy" {
   provisioner "local-exec" {
     command = "ansible-playbook -i inventory/slurm-inventory ansible/headnode.yml --become"
   }
   depends_on = [openstack_compute_floatingip_associate_v2.headnode_floating_ip]
 }
 
-#resource "null_resource" "workernode-ansible-deploy" {
+#resource "null_resource" "workernode-deploy" {
 #  provisioner "local-exec" {
 #    command = "ansible-playbook -i inventory/slurm-inventory ansible/workernode.yml --become"
 #  }
