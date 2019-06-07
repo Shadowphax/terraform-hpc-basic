dd009c7fe7f6a741818d26c286a4545a69a0c268
shadowphax <timothy.carr@uct.ac.za>
2019-06-08 01:36:19 +0200

    No longer require the slurm bits to generate a inventory file.

--- datasources.tf
+++ datasources.tf
@@ -1,27 +1,6 @@
+
 data "openstack_networking_network_v2" "private_net" {
   name = var.pool
-}
-
-//Obtain data from terraform data provider and push to a template file 
-//which closely represents an ansible inventory file. 
-
-data "template_file" "slurm" {
-  template = file("./templates/slurm-inventory.tpl")
-  vars = {
-    headnodename   = openstack_compute_instance_v2.slurm_headnode.name
-    headnodeip     = openstack_compute_instance_v2.slurm_headnode.access_ip_v4
-    controllername = openstack_compute_instance_v2.slurm_controller.name
-    controllerip   = openstack_compute_instance_v2.slurm_controller.access_ip_v4
-    workernodes    = join("\n", openstack_compute_instance_v2.slurm_workers.*.name)
-    workernodesip  = join("\n", openstack_compute_instance_v2.slurm_workers.*.access_ip_v4)
-    jumpbox_floatingIP = openstack_networking_floatingip_v2.floating_ip.address
-    username           = var.ssh_user_name
-  }
-}
-
-resource "local_file" "slurm_file" {
-  content  = data.template_file.slurm.rendered
-  filename = "./inventory/slurm-inventory"
 }
 
 /*
@@ -44,4 +23,32 @@ data "template_file" "groupvars" {
 resource "local_file" "groupvars" {
   content  = data.template_file.groupvars.rendered
   filename = "./ansible/group_vars/all.yml"
+}
+
+resource "local_file" "ansible_inventory" {
+  count = "${length(openstack_compute_instance_v2.slurm_workers.*.name)}"
+  content = "[bastion]\n${join("\n",
+            formatlist(
+              "%s ansible_host=%s",
+              openstack_compute_instance_v2.slurm_headnode.*.name,
+              openstack_networking_floatingip_v2.floating_ip.address
+            ))}\n\n[all:vars]\nansible_user=ubuntu ansible_ssh_common_args='-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no\n\n\n[workernodes]\n${join("\n",
+            formatlist(
+              "%s ansible_host=%s",
+              openstack_compute_instance_v2.slurm_workers.*.name,
+              openstack_compute_instance_v2.slurm_workers.*.access_ip_v4
+            ))}\n\n[workernodes:vars]\nansible_user=ubuntu\n\n\n[headnode]\n${join("\n",
+            formatlist(
+              "%s ansible_host=%s",
+              openstack_compute_instance_v2.slurm_headnode.*.name,
+              openstack_compute_instance_v2.slurm_headnode.*.access_ip_v4
+            )
+            )}\n\n[headnode:vars]\nansible_user=ubuntu\n\n\n[controller]\n${join("\n",
+            formatlist(
+              "%s ansible_host=%s",
+              openstack_compute_instance_v2.slurm_controller.*.name,
+              openstack_compute_instance_v2.slurm_controller.*.access_ip_v4
+            )
+            )}\n\n[controller:vars]\nansible_user=ubuntu"
+  filename = "./inventory/slurm_inventory"
 }
