2c9852d2c5c2d9590a36c95e5d61fecfa9a7bc66
shadowphax <timothy.carr@uct.ac.za>
2019-06-11 22:39:24 +0200

    Removed local provisioners and will only use one instead.

--- provisioner.tf
+++ provisioner.tf
@@ -2,28 +2,28 @@
 
 resource "null_resource" "common-deploy" {
   provisioner "local-exec" {
-    command = "ansible-playbook -i inventory/slurm-inventory ansible/common.yml --become"
+    command = "ansible-playbook -i inventory/slurm-inventory ansible/site.yml --become"
   }
   depends_on = [openstack_compute_floatingip_associate_v2.headnode_floating_ip]
 }
 
-resource "null_resource" "controller-deploy" {
-  provisioner "local-exec" {
-    command = "ansible-playbook -i inventory/slurm-inventory ansible/controller.yml --become"
-  }
-  depends_on = [openstack_compute_floatingip_associate_v2.headnode_floating_ip]
-}
+#resource "null_resource" "controller-deploy" {
+#  provisioner "local-exec" {
+#    command = "ansible-playbook -i inventory/slurm-inventory ansible/controller.yml --become"
+#  }
+#  depends_on = [openstack_compute_floatingip_associate_v2.headnode_floating_ip]
+#}
 
-resource "null_resource" "headnode-deploy" {
-  provisioner "local-exec" {
-    command = "ansible-playbook -i inventory/slurm-inventory ansible/headnode.yml --become"
-  }
-  depends_on = [openstack_compute_floatingip_associate_v2.headnode_floating_ip]
-}
+#resource "null_resource" "headnode-deploy" {
+#  provisioner "local-exec" {
+#    command = "ansible-playbook -i inventory/slurm-inventory ansible/headnode.yml --become"
+#  }
+#  depends_on = [openstack_compute_floatingip_associate_v2.headnode_floating_ip]
+#}
 
-resource "null_resource" "workernode-deploy" {
-  provisioner "local-exec" {
-    command = "ansible-playbook -i inventory/slurm-inventory ansible/workernode.yml --become"
-  }
-  depends_on = [openstack_compute_floatingip_associate_v2.headnode_floating_ip]
-}
+#resource "null_resource" "workernode-deploy" {
+#  provisioner "local-exec" {
+#    command = "ansible-playbook -i inventory/slurm-inventory ansible/workernode.yml --become"
+#  }
+#  depends_on = [openstack_compute_floatingip_associate_v2.headnode_floating_ip]
+#}
