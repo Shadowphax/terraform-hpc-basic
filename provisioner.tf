29b78a1e644cabfcfdb568e4214933edcac023dd
shadowphax <timothy.carr@uct.ac.za>
2019-06-12 09:57:52 +0200

    Added sleep to wait for new cluster servers to settle before connecting. There is a better way to do this though. Must look into ssh connection validation before proceeding.

--- provisioner.tf
+++ provisioner.tf
@@ -4,8 +4,13 @@ resource "null_resource" "common-deploy" {
   triggers = {
     always_run = "${timestamp()}"
   }
+
+  provisioner "local-exec" {
+    command = "sleep 60 & echo \"Sleeping to wait for servers to settle....!\" "
+  }
+
   provisioner "local-exec" {
     command = "ansible-playbook ansible/site.yml --become"
   }
-  depends_on = [openstack_compute_floatingip_associate_v2.headnode_floating_ip]
+  depends_on = [local_file.ansible_inventory]
 }
