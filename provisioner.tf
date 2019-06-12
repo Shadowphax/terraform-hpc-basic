115383ace15a048cd54a9e6b9cc308e3ff482d24
shadowphax <timothy.carr@uct.ac.za>
2019-06-12 11:45:42 +0200

    Reverting delete

--- provisioner.tf
+++ provisioner.tf
@@ -12,5 +12,5 @@ resource "null_resource" "common-deploy" {
   provisioner "local-exec" {
     command = "ansible-playbook ansible/site.yml --become"
   }
-  depends_on = [local_file.ansible_inventory]
+  depends_on = [openstack_compute_floatingip_associate_v2.headnode_floating_ip]
 }
