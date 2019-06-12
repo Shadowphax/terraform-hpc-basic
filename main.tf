28d83cea034ce376dc386fffca3d5dc20b3dd445
shadowphax <timothy.carr@uct.ac.za>
2019-06-12 15:21:38 +0200

    Added additional volume to headnode for NFS

--- main.tf
+++ main.tf
@@ -109,6 +109,14 @@ resource "openstack_compute_instance_v2" "slurm_headnode" {
     delete_on_termination = false
   }
 
+  block_device {
+    uuid                  = openstack_blockstorage_volume_v3.slurm_headnode_nfs_data.id
+    source_type           = "volume"
+    boot_index            = 1
+    destination_type      = "volume"
+    delete_on_termination = false
+  }
+
   network {
     uuid = openstack_networking_network_v2.private_net.id
   }
