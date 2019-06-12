e0f6cd966dc728fa47fc5ec586e928a214ca1a7d
shadowphax <timothy.carr@uct.ac.za>
2019-06-12 21:12:35 +0200

    Remove NFS volumes. No longer needed. Will implement BeeGFS instead.

--- volumes.tf
+++ volumes.tf
@@ -17,12 +17,3 @@ resource "openstack_blockstorage_volume_v3" "slurm_ctl_boot" {
   image_id    = var.image
   enable_online_resize = true
 }
-
-# Create Slurm Headnode NFS Export Volume 
-
-resource "openstack_blockstorage_volume_v3" "slurm_headnode_nfs_data" {
-  name        = "Slurm_nfs_data"
-  description = "Slurm Headnode NFS Data Export Volume"
-  size        = 5
-  enable_online_resize = true
-}
