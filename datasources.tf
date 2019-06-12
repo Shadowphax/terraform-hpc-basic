a41827490ad86a3099702f8a83349a86f0452502
shadowphax <timothy.carr@uct.ac.za>
2019-06-12 23:52:48 +0200

    Added additional BeeGFS hosts for inventory

--- datasources.tf
+++ datasources.tf
@@ -57,6 +57,18 @@ resource "local_file" "ansible_inventory" {
               openstack_compute_instance_v2.slurm_controller.*.name,
               openstack_compute_instance_v2.slurm_controller.*.access_ip_v4
             )
-            )}\n\n[controller:vars]\nansible_user=${var.ssh_user_name}"     
+            )}\n\n[controller:vars]\nansible_user=${var.ssh_user_name}\n\n[leader]\n${join("\n",
+            formatlist(
+              "%s ansible_host=%s",
+              openstack_compute_instance_v2.beegfs_storage_1.*.name,
+              openstack_compute_instance_v2.beegfs_storage_1.*.access_ip_v4
+            )
+            )}\n\n[leader:vars]\nansible_user=${var.ssh_user_name}\n\n[follower]\n${join("\n",
+            formatlist(
+              "%s ansible_host=%s",
+              openstack_compute_instance_v2.beegfs_storage_2.*.name,
+              openstack_compute_instance_v2.beegfs_storage_2.*.access_ip_v4
+            )
+            )}\n\n[follower:vars]\nansible_user=${var.ssh_user_name}\n\n[cluster:children]\nleader\nfollower\n\n[cluster_beegfs_mgmt:children]\nleader\n\n[cluster_beegfs_mds:children]\nleader\n\n[cluster_beegfs_oss:children]\nleader\nfollower\n\n[cluster_beegfs_client:children]\nleader\nfollower\n\n"            
   filename = "./inventory/slurm-inventory"
 }
